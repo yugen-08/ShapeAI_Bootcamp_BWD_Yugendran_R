fea8f78614fda161810aa22211a075398039222b
yugen-08 <80707056+yugen-08@users.noreply.github.com>
2021-06-05 18:45:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+ return (
+  <div>
+    <h1>Javascript and React.js</h1>
+    <p>
+      This was an amazing bootcamp taken up ny Shaurya sir we covered 
+      everything from Scratch including including Javascript React.js, HTML.
+    </p>
+  </div>
+ );
+}
+
+export default Note;
